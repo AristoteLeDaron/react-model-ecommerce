6e1c06af4ef09b09cb8e0a21ae58900f4b1d3472
AristoteLeDaron <aristotenataf@gmail.com>
2023-10-23 10:45:16 +0200

    ajout de components, et de Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header style={{ backgroundColor: 'var(--primary-color)', padding: '1rem' }}>
+      <h1 style={{ color: 'var(--background-color)' }}>React Model E-Commerce</h1>
+    </header>
+  );
+}
+
+export default Header;
