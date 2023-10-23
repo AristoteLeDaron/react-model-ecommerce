f385db25ae4609b8b0b3b7b19a8e5986bbd6748e
AristoteLeDaron <aristotenataf@gmail.com>
2023-10-23 10:50:42 +0200

    ajout des quelques routes principales

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function HomePage() {
+  return <div>Accueil</div>;
+}
+
+function ProductsPage() {
+  return <div>Produits</div>;
+}
+
+function LoginPage() {
+  return <div>Connexion</div>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/produits" element={<ProductsPage />} />
+          <Route path="/connexion" element={<LoginPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
