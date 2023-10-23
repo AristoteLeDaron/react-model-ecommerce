98cddaf2103d810ca68aa2a042865763771bc077
AristoteLeDaron <aristotenataf@gmail.com>
2023-10-23 15:20:38 +0200

    ajout d'une barre de recherche d'un bouton de connexion et d'authentification et d'un bouton panier dans le header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import NavBar from '../NavBar/NavBar';  // Assurez-vous d'avoir le bon chemin d'importation pour NavBar.
 import './Header.css';
-import NavBar from '../NavBar/NavBar';
 
 function Header() {
   return (
     <header className="header">
       <div className="headerContainer">
         <h1>React Model E-Commerce</h1>
-        <NavBar />
+        <input type="text" placeholder="Recherchez votre produit ici" className="searchInput" />
+        <button className="searchBtn">🔍</button>
+        <button className="loginBtn">Se connecter / S'enregistrer</button>
+        <button className="cartBtn">
+          🛒 Panier / $0.00
+          {/* Remplacez $0.00 par la valeur dynamique du panier */}
+        </button>
       </div>
+      <NavBar />
     </header>
   );
 }
