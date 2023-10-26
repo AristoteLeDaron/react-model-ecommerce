869969724c0f4bffdd2eef84b5389ab3a369891f
AristoteLeDaron <aristotenataf@gmail.com>
2023-10-26 14:50:34 +0200

    mise a jour de ProductDetailsCard.js et de son css

--- src/components/ProductDetailsCard/ProductDetailsCard.js
+++ src/components/ProductDetailsCard/ProductDetailsCard.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './ProductDetailsCard.css';  // Assurez-vous de définir votre CSS ici si nécessaire.
+import './ProductDetailsCard.css';  // Assurez-vous d'importer le bon fichier CSS
 
 function ProductDetailsCard({ product }) {
   return (
     <div className="product-card">
-      <img src={product.imageUrl} alt={product.name} className="product-card__image" />
+      <img src={product.image} alt={product.name} className="product-card__image" />
       <h2 className="product-card__name">{product.name}</h2>
       <p className="product-card__price">{product.price}€</p>
     </div>
